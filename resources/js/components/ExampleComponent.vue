c9a2dfa1aed5115d9dbf0f747e418f03fe9efb00
AFS12 <arthurmbayres@gmail.com>
2020-10-21 11:00:24 -0300

    vue ambientado

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm an example component.
+                        <input type="text" v-model="test">
+                        <br>
+                        {{test}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: () => {
+            return {
+                test:''
+        }},
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
